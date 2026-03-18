73bf5300d863f1a055eab7245ea2f8feaedd7560
devdilyorbek <dilyorbekdev@gmail.com>
2026-03-18 13:25:04 +0500

    Initial commit from Create Next App

---
+++ constants/index.ts
@@ -0,0 +1,11 @@
+export const navLinks = [
+  { name: "Home", path: "/" },
+];
+
+export const siteDetails = {
+  title: "ChefAI",
+  sub: "Elevate Your Mealtime with AI-Powered Personalization",
+  author: "Dilyorbek Asfandiyorov",
+  description: "ChefAI is an AI-powered recipe generator that helps you create personalized recipes based on your dietary preferences and restrictions.",
+  url: "",
+};
